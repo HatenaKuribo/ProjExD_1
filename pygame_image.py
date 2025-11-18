5d9cd690f485311dce75e66e4f48efa2f29584c0
Kuribo <c0a24009dc@edu.teu.ac.jp>
2025-11-18 15:25:11 +0900

    練習6:FPSの変更,背景画像の移動に使う変数の使い方を変更

--- pygame_image.py
+++ pygame_image.py
@@ -9,6 +9,7 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+    clock.tick(200)
     bg_img = pg.image.load("fig/pg_bg.jpg")
     player_img = pg.image.load("fig/3.png")
     player_img = pg.transform.flip(player_img, True, False)
@@ -18,11 +19,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img, [-x, 0])
         screen.blit(player_img, [300,200])
         pg.display.update()
         tmr += 1 
-        x -= 1       
+        x += 1       
         clock.tick(10)
 
 
